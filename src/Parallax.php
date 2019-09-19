0c802962b627f07aaf05f73054fc61339a344ca9
wpsmith <t@wpsmith.net>
2019-09-18 21:51:03 -0400

    Fix namespace

--- src/Parallax.php
+++ src/Parallax.php
@@ -10,14 +10,14 @@
  *
  * @package    WPS\Core
  * @author     Travis Smith <t@wpsmith.net>
- * @copyright  2015-2018 Travis Smith
+ * @copyright  2015-2019 Travis Smith
  * @license    http://opensource.org/licenses/gpl-2.0.php GNU Public License v2
  * @link       https://github.com/wpsmith/WPS
  * @version    1.0.0
  * @since      0.1.0
  */
 
-namespace WPS\Customizer;
+namespace WPS\WP\Customizer;
 
 use WPS\Scripts;
 
@@ -26,7 +26,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-if ( ! class_exists( 'Parallax' ) ) {
+if ( ! class_exists( __NAMESPACE__ . '\Parallax' ) ) {
 	/**
 	 * Class Parallax
 	 *
